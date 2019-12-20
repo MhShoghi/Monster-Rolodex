ee7aa278f0cf8ea447c807edb30b3b005e0227c7
mhshledomi <hoseinshoghi@gmail.com>
2019-12-20 14:35:11 +0330

    second commit

--- src/components/card/card.componnet.jsx
+++ src/components/card/card.componnet.jsx
@@ -5,7 +5,7 @@ export const Card = props => {
     return (
         <div className="card-container">
             <img src={`https://robohash.org/${props.monster.id}?set=set1&size=180x180`} alt=""/>
-            <h1>{props.monster.name}</h1>
+            <p>{props.monster.name}</p>
             <p>{props.monster.email}</p>
         </div>
     )
