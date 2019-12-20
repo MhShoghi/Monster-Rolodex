b32e165767fefa65cee335adfd0dcaf5af2455da
mhshledomi <hoseinshoghi@gmail.com>
2019-12-20 14:33:22 +0330

    Updates

---
+++ precache-manifest.7b739443a99612a59d1acadbd9bdd6ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d892588310bb812a75524808c1b1e67d",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "8ffd96056e9d4b2bc752",
+    "url": "/Monster-Rolodex/static/css/main.ae08560b.chunk.css"
+  },
+  {
+    "revision": "8ba0a62509b045ff292e",
+    "url": "/Monster-Rolodex/static/js/2.1d888330.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monster-Rolodex/static/js/2.1d888330.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ffd96056e9d4b2bc752",
+    "url": "/Monster-Rolodex/static/js/main.754f2d59.chunk.js"
+  },
+  {
+    "revision": "f00b5701c845d672da7b",
+    "url": "/Monster-Rolodex/static/js/runtime-main.27f9cd65.js"
+  }
+]);
